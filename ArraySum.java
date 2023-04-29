ad84eeb93468b7fd97139564e4c6fed442ee2fa7
Abhishek <abhishek.coolabhi@gmail.com>
2023-04-29 18:29:23 +0530

    Array Sum program is written

---
+++ ArraySum.java
@@ -0,0 +1,21 @@
+public class ArraySum{
+
+public static void main (String abhi[])
+{
+
+int sum = 0;
+
+int a[] = {1,2,3,4,5};
+
+for (int i = 0; i<a.length; i++)
+
+sum = sum+a[i];
+
+System.out.println("The sum of the Array Elements is " + sum);
+
+
+}
+
+
+
+}
