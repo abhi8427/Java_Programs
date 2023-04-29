ac51b91455375071fa0da62b1013d55afd24a385
Abhishek <abhishek.coolabhi@gmail.com>
2023-04-29 17:06:51 +0530

    my first program

---
+++ ArrayPrint.java
@@ -0,0 +1,18 @@
+public class ArrayPrint{
+
+public static void main(String abhi[])
+{
+
+int a[] = {1,2,3,4,5,6,7,8,9};
+
+
+for(int i = 0; i<a.length; i++)
+
+
+System.out.println(a[i]);
+
+
+}
+
+
+}
