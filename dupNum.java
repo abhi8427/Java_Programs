6c66e05b41a118a6ddef27ac155c4e75ac6c1435
Abhishek <abhishek.coolabhi@gmail.com>
2023-05-01 23:53:42 +0530

    this is for identifying duplicate number in array:wq
    :wq
    :i
    i
    :wq

---
+++ dupNum.java
@@ -0,0 +1,16 @@
+public class dupNum
+{
+public static void main(String abhi[])
+{
+int a[] = {1,2,3,1,4,1};
+
+for (int i = 0; i<a.length;i++)
+{
+for(int j = 1;j<a.length;j++)
+{
+if(a[i] == a[j])
+System.out.println("Duplicate number is --- " +a[j]);
+}
+}
+}
+}
