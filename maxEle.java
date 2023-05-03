a67e68209ba82e9481bbf6b3f5301231b6e08394
Abhishek <abhishek.coolabhi@gmail.com>
2023-05-03 23:41:51 +0530

    program to identify max element in given array :wq

---
+++ maxEle.java
@@ -0,0 +1,16 @@
+public class maxEle
+{
+public static void main(String abhi[])
+{
+int max=0;
+int a[] = {1,2,3,4,5};
+for(int i = 0;i<a.length;i++)
+if(max<a[i])
+{
+max = a[i];
+System.out.println("Heightest Number is ---" +max);
+}
+
+}
+
+}
