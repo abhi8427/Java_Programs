fa01d4b44a7bb36d91d7a5d6ad91532fa964cd69
Abhishek <abhishek.coolabhi@gmail.com>
2023-05-07 18:13:50 +0530

    Identify the duplicate element in a given array

--- dupNum.java
+++ dupNum.java
@@ -6,11 +6,13 @@ int a[] = {1,2,3,1,4,1};
 
 for (int i = 0; i<a.length;i++)
 {
-for(int j = 1;j<a.length;j++)
+for(int j = i+1;j<a.length;j++)
 {
 if(a[i] == a[j])
 System.out.println("Duplicate number is --- " +a[j]);
 }
+
+
 }
 }
 }
