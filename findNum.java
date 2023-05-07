e55ab9fa379e0f06c7f1df30dd8c16db6d4f3b78
Abhishek <abhishek.coolabhi@gmail.com>
2023-05-07 18:36:01 +0530

    updated the program to identify the given number

--- findNum.java
+++ findNum.java
@@ -3,14 +3,11 @@ public class findNum
 public static void main(String abhi[])
 {
 int a[] = {1,2,3,4,5};
-int num = 4;
-
+int num = 3;
 for(int i = 0;i<a.length;i++)
 {
 if(a[i] == num)
 System.out.println(a[i]);
 }
-
 }
-
 }
