b83fafded01bd3faf6ce04350e9e259c1b9548f2
Abhishek <abhishek.coolabhi@gmail.com>
2023-06-22 23:37:08 +0530

    add three number

---
+++ AddThree.java
@@ -0,0 +1,29 @@
+import java. util. *; 
+
+public class AddThree{
+
+public static void main(String abhi[]){
+
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Enter First Number");
+
+int a = scan.nextInt();
+
+
+System.out.println("Enter second number");
+int b = scan.nextInt();
+
+
+System.out.println("Enter 3rd Number");
+int c = scan.nextInt();
+
+System.out.println("Addition is ");
+System.out.println(a+b+c);
+
+
+}
+
+
+
+}
