ce13e5b4a3abfbbb881589e6f2f8222b50658fd3
Abhishek <abhishek.coolabhi@gmail.com>
2023-06-22 23:27:24 +0530

    Add two numbers

---
+++ Adding.java
@@ -0,0 +1,23 @@
+import java. util. *; 
+
+public class Adding{
+
+public static void main(String args[])
+{
+
+Scanner scan = new Scanner(System.in);
+
+System.out.println("Enter first number");
+int a = scan.nextInt();
+
+
+System.out.println("Enter 2nd Number");
+
+int b = scan.nextInt();
+
+System.out.println(a+b);
+
+}
+
+
+}
