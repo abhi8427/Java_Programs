4439646d9de88241db936ac0c3a126c8cc0953bc
Abhishek <abhishek.coolabhi@gmail.com>
2023-06-24 23:33:00 +0530

    India

---
+++ addNum1.java
@@ -0,0 +1,23 @@
+import java. util. *;
+
+public class addNum1{
+
+public static void main(String abhi[])
+{
+
+Scanner scan = new Scanner(System.in);
+
+int a = scan.nextInt();
+int b = scan.nextInt();
+
+System.out.println(a+b);
+
+
+
+
+}
+
+
+
+
+}
