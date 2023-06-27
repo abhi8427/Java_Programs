3f004f59b4d47a9662eaa424889c193227e8015f
Abhishek <abhishek.coolabhi@gmail.com>
2023-06-27 23:45:05 +0530

    scanner class program

---
+++ addNum.java
@@ -0,0 +1,20 @@
+import java. util. *;
+
+public class addNum{
+
+public static void main(String args[]){
+
+Scanner scan = new Scanner(System.in);
+System.out.println("ENter a Number");
+int a = scan.nextInt();
+
+
+System.out.println("Enter your Name");
+String Name = scan.nextLine();
+System.out.println(Name);
+
+
+}
+
+
+}
