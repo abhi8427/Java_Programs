2c819056e383f1c5c60068debbd73e2464acaf46
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-04 23:43:44 +0530

    Age Calculator

---
+++ AgeCalc.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class AgeCalc{
+
+public static void main(String args[]){
+
+LocalDate today = LocalDate.now();
+
+LocalDate birthdate = LocalDate.of(1982, 05, 22);
+
+int years = Period.between(birthdate,today).getYears();
+
+System.out.println(years);
+
+
+}
+
+
+
+}
