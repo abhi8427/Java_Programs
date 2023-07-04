816e318ba68f6e8c13ae87ef796b56a411170889
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-04 23:51:12 +0530

    user name input

---
+++ UserName.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class UserName{
+
+public static void main(String abhishek[]){
+
+Scanner scan = new Scanner(System.in);
+System.out.println("Please enter your name");
+String name = scan.nextLine();
+
+
+
+System.out.println(name);
+
+}
+
+}
