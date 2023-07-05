480224af3647cc147fcec02b7f08f4e499cd9635
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-05 23:56:38 +0530

    Akshita Age

--- AgeCalc1.java
+++ AgeCalc1.java
@@ -7,7 +7,7 @@ public static void main(String abhi[]){
 
 LocalDate today = LocalDate.now();
 
-LocalDate birthdate = LocalDate.of(1982, 5, 22);
+LocalDate birthdate = LocalDate.of(1988, 8, 4);
 
 int years = Period.between(birthdate, today).getYears();
 System.out.println(years);
