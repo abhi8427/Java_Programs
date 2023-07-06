0bc4ddfe9a4d0d5c5f89191ab5c57df68b5ab469
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-06 23:11:03 +0530

    Name input program

---
+++ enterName.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class enterName{
+
+public static void main(String abhi[])
+{
+
+
+System.out.println("Enter any Name");
+
+Scanner scan = new Scanner(System.in);
+
+
+
+String Name = scan.nextLine();
+
+System.out.println("Entered name from the user is " + Name);
+
+}
+
+
+}
