541f81a61724837436df7556239c5ac4363f8a23
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-07 23:52:03 +0530

    java program

---
+++ evenOdd.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class evenOdd{
+
+public static void main(String abhi[]){
+
+System.out.println("Please enter the number");
+
+Scanner scan = new Scanner(System.in);
+
+//int num = scan.nextInt();
+
+int num = 4;
+
+if(num%2=0){
+System.out.println("Number is even");
+}else if(num%2!=0)
+{
+System.out.println("Number is odd");
+}
+
+}
+
+}
