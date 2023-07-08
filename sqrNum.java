25aa2ac0ea0156a34dbe3eea049280c93aded486
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-08 23:54:23 +0530

    Square of a given number

---
+++ sqrNum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class sqrNum{
+
+public static void main(String ab[]){
+System.out.println("enter a number");
+
+Scanner scan = new Scanner(System.in);
+
+int num = scan.nextInt();
+
+System.out.println("Square of the number is " + num*num);
+
+}
+
+
+}
