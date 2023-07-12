cd70f518437ca56b97908c5ead2446897b16c403
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-12 23:52:31 +0530

    number program

---
+++ enterNumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class enterNumber{
+
+public static void main(String ab[]){
+
+System.out.println("Please enter any number");
+
+Scanner scan = new Scanner(System.in);
+
+int num = scan.nextInt();
+
+System.out.println("You have enter number " +num);
+
+
+
+
+}
+}
