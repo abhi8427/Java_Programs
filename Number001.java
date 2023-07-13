60ad1a7a094a04ab7422bffe9923211ac30641b4
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-13 23:30:02 +0530

    number comparison program

---
+++ Number001.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Number001{
+
+public static void main(String abhi[]){
+
+System.out.println("Please enter first number");
+
+Scanner scan = new Scanner(System.in);
+
+int num1 = scan.nextInt();
+
+System.out.println("Please enter second number");
+
+int num2 = scan.nextInt();
+
+if(num1>num2){
+
+System.out.println(num1 + " is greater than " + num2);
+}else
+{
+System.out.println(num2 + " is greater than " + num1);
+}
+
+
+}
+}
