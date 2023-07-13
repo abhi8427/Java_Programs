f1ee2aa5bb7075ff8ff9d42ece582f0c369220ca
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-13 23:33:11 +0530

    number comparison

--- Number001.java
+++ Number001.java
@@ -17,9 +17,12 @@ int num2 = scan.nextInt();
 if(num1>num2){
 
 System.out.println(num1 + " is greater than " + num2);
-}else
+}else if(num2>num1)
 {
 System.out.println(num2 + " is greater than " + num1);
+}else if(num1 == num2)
+{
+System.out.println(num1 + " and " + num2 + " are equal");
 }
 
 
