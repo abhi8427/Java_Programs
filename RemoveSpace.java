a27d30788452a3d9d1935b7e988d63197033c688
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-14 23:42:48 +0530

    Remove spaces from given string

--- RemoveSpace.java
+++ RemoveSpace.java
@@ -4,9 +4,11 @@ public static void main(String ab[]){
 
 String str = "A b h i s h e k";
 
+System.out.println("Before String :- " +str);
+
 str = str.replaceAll("\\s", "");
 
-System.out.println(str);
+System.out.println("After String :- " +str);
 
 
 }
