c1d3a01c278970af5398b4cecff985f66c42e487
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-15 23:47:15 +0530

    Enter Name Program

---
+++ EnterName01.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class EnterName01{
+
+public static void main(String ab[]){
+
+Scanner scan = new Scanner(System.in);
+
+String name = scan.nextLine();
+
+System.out.println("Your name is " + name);
+
+
+}
+
+}
