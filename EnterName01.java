d9e5e4017ec5d7b6cfbf2806fa707b6584fc63ee
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-15 23:50:39 +0530

    enter name program

--- EnterName01.java
+++ EnterName01.java
@@ -4,6 +4,8 @@ public class EnterName01{
 
 public static void main(String ab[]){
 
+System.out.print("Please enter your name");
+
 Scanner scan = new Scanner(System.in);
 
 String name = scan.nextLine();
