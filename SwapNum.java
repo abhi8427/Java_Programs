391e6d28d9e64b4edc4409f59ecb20223ff27be7
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-17 23:41:18 +0530

    Swapping NUmber

---
+++ SwapNum.java
@@ -0,0 +1,22 @@
+
+public class SwapNum{
+
+public static void main(String ab[]){
+
+int a = 10, b = 20;
+
+System.out.println("Number before swapping" + a + " " + b);
+
+int t = a;
+
+a = b;
+
+b = t;
+
+
+System.out.println("Number before swapping" + a + " " + b);
+
+
+}
+
+}
