b091ebdec925f81f4f3b2a2a534975d6eda61a3e
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-18 22:40:37 +0530

    swap number

---
+++ SwapNum02.java
@@ -0,0 +1,18 @@
+public class SwapNum02{
+public static void main(String ab[]){
+
+int a = 10, b = 20;
+
+System.out.println("The number before swap " + a + " " + b);
+
+a = a*b;
+b = a/b;
+a = a/b;
+
+System.out.print("The NUmber after swap " + a + " " + b);
+
+
+
+
+}
+}
