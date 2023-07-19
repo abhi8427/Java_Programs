db663973509d3538930641227b43eea3e33a1508
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-19 23:42:12 +0530

    swap of number

---
+++ SwapNum04.java
@@ -0,0 +1,22 @@
+public class SwapNum04{
+
+public static void main(String ab[]){
+
+int a = 10, b = 20;
+
+System.out.println("Number before swap " + a + " " + b );
+
+int t = a;
+
+a = b;
+
+b = t;
+
+System.out.println("Number after swap " + a + " " + b);
+
+
+
+
+
+}
+}
