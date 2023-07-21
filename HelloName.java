7d0c7ab4d4b468f827fcf0989c5538ebf081d350
Abhishek <abhishek.coolabhi@gmail.com>
2023-07-21 23:56:18 +0530

    Hello World Program

---
+++ HelloName.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class HelloName{
+
+public static void main(String ab[]){
+
+System.out.println("Please enter your name");
+
+Scanner scan = new Scanner(System.in);
+
+String name = scan.nextLine();
+
+System.out.println("Hello World, This is " + name);
+
+}
+
+}
