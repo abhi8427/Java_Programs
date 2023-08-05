7ff8dab37755b7f006738c591660d0262634dd89
Abhishek <abhishek.coolabhi@gmail.com>
2023-08-05 23:48:02 +0530

    array

--- array1.java
+++ array1.java
@@ -3,7 +3,7 @@ public class array1{
 public static void main(String abhi[])
 {
 
-int a [] = {1,2,3,4,5,6,7,8,9};
+int a [] = {1,2,3,4,5,6,7};
 
 for(int i = 0;i<a.length;i++){
 System.out.println(a[i]);
