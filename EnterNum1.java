7eb69404c0e2140a722467eb31304e24854768a5
Abhishek <abhishek.coolabhi@gmail.com>
2023-08-07 23:50:06 +0530

    enter num program

---
+++ EnterNum1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class EnterNum1{
+
+public static void main(String ab[]){
+
+System.out.println("Enter any number");
+
+Scanner scan = new Scanner(System.in);
+int num = scan.nextInt();
+
+System.out.println("Your entered number is " + num);
+
+
+}
+
+
+}
