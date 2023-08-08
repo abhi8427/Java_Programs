1d0a4f2c5099f92d29924261b1004eabd3baae02
Abhishek <abhishek.coolabhi@gmail.com>
2023-08-08 23:45:06 +0530

    name and number

---
+++ NumName.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class NumName{
+
+public static void main(String ab[]){
+
+System.out.print("Please enter any name");
+
+Scanner scan = new Scanner(System.in);
+
+String name = scan.nextLine();
+
+System.out.println("Entered name is " +name);
+
+
+
+
+
+}
+
+}
