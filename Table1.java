9edba6ae359be7ffa28ddb0ddceb2f4e1f089e9a
Abhishek <abhishek.coolabhi@gmail.com>
2023-08-10 23:55:35 +0530

    table from number

---
+++ Table1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Table1{
+
+public static void main(String ab[]){
+
+System.out.println("Please enter any number");
+
+Scanner scan = new Scanner(System.in);
+
+int num = scan.nextInt();
+
+System.out.println("Your Table number starts from here");
+
+for(int i = 1;i<=10;i++)
+
+System.out.println(num*i);
+
+
+}
+
+}
