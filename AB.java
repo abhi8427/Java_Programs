5207a7a70d0752923b910f1a4ff6befb48402aae
Abhishek <abhishek.coolabhi@gmail.com>
2023-08-12 23:53:47 +0530

    name change

--- AB.java
+++ AB.java
@@ -2,7 +2,7 @@ public class AB
 {
 public static void main(String abhi[])
 {
-System.out.println("Hi There, I am Abhishek Kumar Sharma from Kanpur, Uttar Pradesh. Currently, I am staying in Pune. My Father's name is Late Mr. Shashi Kant Sharma");
+System.out.println("Hi There, I am Abhishek Kumar Sharma from Kanpur, Uttar Pradesh. Currently, I am staying in Pune. My Father's name is Late Mr. Shashi Kant Sharma. My Mother's name is Mrs. Shashi Sharma");
 }
 
 
