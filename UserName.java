0f827de20d15ed5877e2bd43396bcb06de6af01b
Abhishek <abhishek.coolabhi@gmail.com>
2023-08-15 23:54:52 +0530

    user name

--- UserName.java
+++ UserName.java
@@ -7,9 +7,6 @@ public static void main(String abhishek[]){
 Scanner scan = new Scanner(System.in);
 System.out.println("Please enter your full name");
 String name = scan.nextLine();
-
-
-
 System.out.println(name);
 
 }
