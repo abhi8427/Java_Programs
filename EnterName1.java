ffe38639684a6fe0a88b79b82f88b2ccc8e452d3
Abhishek <abhishek.coolabhi@gmail.com>
2023-08-23 23:51:27 +0530

    name print program

--- EnterName1.java
+++ EnterName1.java
@@ -4,7 +4,7 @@ public class EnterName1{
 
 public static void main(String ab[]){
 
-System.out.println("Please Enter your Name here");
+System.out.println("Please Enter your Name");
 
 Scanner scan = new Scanner(System.in);
 
