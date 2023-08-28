0e1fd802e06e87d7260fec870129de83e4a8fc1a
Abhishek <abhishek.coolabhi@gmail.com>
2023-08-28 23:53:49 +0530

    remove space program

--- RemoveSpace.java
+++ RemoveSpace.java
@@ -2,7 +2,7 @@ public class RemoveSpace{
 
 public static void main(String ab[]){
 
-String str = "A b h i s h e k";
+String str = "A b h i s h e k k u m a r";
 
 System.out.println("Before String :- " +str);
 
