5877e00f3629c23af67fdb8c6cd8c69d7ff774df
Abhishek <abhishek.coolabhi@gmail.com>
2023-08-31 23:53:52 +0530

    pattern program

---
+++ patPro.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class patPro{
+
+public static void main(String ab[]){
+
+System.out.println("Please enter number of rows");
+
+Scanner scan = new Scanner(System.in);
+
+int rownum = scan.nextInt();
+
+}
+
+}
