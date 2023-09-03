2996afbce5805f186dd87fb654e0204650a7fda5
Abhishek <abhishek.coolabhi@gmail.com>
2023-09-03 23:25:57 +0530

    pattern program

---
+++ patProg002.java
@@ -0,0 +1,21 @@
+public class patProg002{
+
+public static void main(String ab[]){
+
+int rownum = 5;
+
+for(int row = 1; row<=rownum; row++)
+{
+for(int col = 1; col<=row; col++)
+{
+System.out.print("*");
+System.out.print("|");
+
+}
+System.out.println();
+
+}
+
+}
+
+}
