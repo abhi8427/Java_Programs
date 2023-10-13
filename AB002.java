93ea7588ee222a0c1b79081118adf71cf30df7c6
Abhishek <abhishek.coolabhi@gmail.com>
2023-10-13 23:51:25 +0530

    name program

--- AB002.java
+++ AB002.java
@@ -2,7 +2,7 @@ public class AB002{
 
 public static void main(String ab[]){
 
-System.out.println("I am from Kanpur is the best city");
+System.out.println("I am from Kanpur uttar pradesh is the best city");
 
 }
 
