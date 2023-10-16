9e6a8f2e9ba16fbf1ad7d5ff278793b66f3bea37
Abhishek <abhishek.coolabhi@gmail.com>
2023-10-16 23:56:55 +0530

    number count

---
+++ numCount.java
@@ -0,0 +1,20 @@
+public class numCount{
+
+public static void main(String ab[]){
+
+int num = 12345;
+
+int count = 0;
+
+while(num>0)
+{
+num = num/10;
+count++;
+}
+
+System.out.println(count);
+
+}
+
+
+}
