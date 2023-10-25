e0138c5bec233c8438846573dadd14b322d1b68a
Abhishek <abhishek.coolabhi@gmail.com>
2023-10-25 23:53:19 +0530

    dog program

---
+++ Dog.java
@@ -0,0 +1,15 @@
+public class Dog extends Animal{
+
+String Name = "Purple";
+
+public static void main(String ab[]){
+
+
+Dog d1 = new Dog();
+d1.color = "Yellow";
+
+System.out.println(d1.color);
+
+}
+
+}
