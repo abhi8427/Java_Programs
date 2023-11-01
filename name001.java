3fb4c8ecbb633576d9214095e41243c72dfe8814
Abhishek <abhishek.coolabhi@gmail.com>
2023-11-01 23:45:10 +0530

    name program

---
+++ name001.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class name001{
+
+public static void main(String ab[])
+{
+
+Scanner scan = new Scanner(System.in);
+
+System.out.println("ENter your name");
+
+String name = scan.nextLine();
+
+System.out.println("Your name is " + name);
+
+
+
+
+
+
+
+}
+
+}
