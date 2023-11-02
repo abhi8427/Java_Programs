77341a457d3c81bf5e0b4c92fb27e127408fb198
Abhishek <abhishek.coolabhi@gmail.com>
2023-11-02 23:55:39 +0530

    number program

---
+++ num003.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class num003{
+
+public static void main(String ab[]){
+
+System.out.println("enter your number");
+
+Scanner scan = new Scanner(System.in);
+
+int num = scan.nextInt();
+
+System.out.println("your entered number is " + num);
+
+
+
+
+
+
+}
+}
