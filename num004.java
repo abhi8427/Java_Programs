4ad03de42b43feb7ef09bf0130e88b1edb589d95
Abhishek <abhishek.coolabhi@gmail.com>
2023-11-03 23:43:34 +0530

    number program

---
+++ num004.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class num004{
+
+public static void main(String ab[]){
+
+System.out.println("Enter your number");
+
+Scanner scan = new Scanner(System.in);
+
+int num = scan.nextInt();
+
+System.out.println("your entered number is " + num);
+
+}
+}
