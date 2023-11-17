dbb91475739028cd9cfa16452f4aa4cd42cd5434
Abhishek <abhishek.coolabhi@gmail.com>
2023-11-17 23:38:57 +0530

    name program

--- name005.java
+++ name005.java
@@ -6,8 +6,11 @@ public static void main(String ab[])
 {
 
 System.out.println("Please enter your name");
+
 Scanner scan = new Scanner(System.in);
+
 String name = scan.nextLine();
+
 System.out.println("Your entered name is " +name);
 
 }
