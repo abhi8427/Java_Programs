201260367590ccc97cf3b33d05e37fe530a82c2d
Abhishek <abhishek.coolabhi@gmail.com>
2024-12-22 10:40:48 +0530

    Number print

---
+++ NumberPrint.java
@@ -0,0 +1,18 @@
+package sevenDecember;
+
+public class NumberPrint {
+	
+	public static void main(String ab[]){
+		NumberPrint np = new NumberPrint();
+		np.num();
+		
+	}
+
+	public void num(){
+		System.out.println("Number from 1 to 10 is");
+	for(int i = 1; i<=10;i++){
+		
+		System.out.println(i);
+	}
+	}
+}
