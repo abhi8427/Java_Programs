672960564115c0fb026b0485bb07dca191e81d24
Abhishek <abhishek.coolabhi@gmail.com>
2025-01-17 23:48:23 +0530

    Sum of two number

--- SumofNum.java
+++ SumofNum.java
@@ -1,12 +1,26 @@
 package JANUARY2025.Java_Programs;
 
+import java.util.Scanner;
+
 public class SumofNum {
 	
 	public static void main(String ab[]){
-		int a  = 1;
-		int b = 2;
-		int c = a+b;
-		System.out.println("The sum of two numbers is " +c);
+		
+		sum();
+
+
+	}
+	
+	public static void sum(){
+		System.out.println("Please enter first number");
+		Scanner scan = new Scanner(System.in);
+		int num1 = scan.nextInt();
+		
+		System.out.println("Please enter second number");
+		int num2 = scan.nextInt();
+		int sum = num1+num2;
+		
+		System.out.println("The sum of two numbers is " + sum);
 	}
 	
 	
