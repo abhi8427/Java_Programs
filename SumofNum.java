c770b6720e586cbbc8fb46f68827a153aa7c6547
Abhishek <abhishek.coolabhi@gmail.com>
2025-01-17 23:45:08 +0530

    number sum

---
+++ SumofNum.java
@@ -0,0 +1,14 @@
+package JANUARY2025.Java_Programs;
+
+public class SumofNum {
+	
+	public static void main(String ab[]){
+		int a  = 1;
+		int b = 2;
+		int c = a+b;
+		System.out.println("The sum of two numbers is " +c);
+	}
+	
+	
+
+}
