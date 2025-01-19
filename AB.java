d2e46adeeadd7e13a3f4bbe6c7bb62b2be7b3ad3
Abhishek <abhishek.coolabhi@gmail.com>
2025-01-19 23:52:02 +0530

    simple program

--- AB.java
+++ AB.java
@@ -5,7 +5,8 @@ public class AB
 public static void main(String abhi[])
 {
 System.out.println("Hello All, There, I am Abhishek Kumar from kanpur, Uttar Pradesh");
-System.out.println("I am learning new skills");
+System.out.println("I am learning new skills ");
+System.out.println("I will do 12 certifications this year");
 }
 
 
